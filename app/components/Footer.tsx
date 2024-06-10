16c85b1460c20f316a4c087811306f5a82b079b1
kevinnaserwan <kevinnaserwan@gmail.com>
2024-06-10 15:26:27 +0700

    Fix: bug footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -6,15 +6,6 @@ import MagicButton from "./MagicButton";
 const Footer = () => {
   return (
     <footer className="w-full pt-20 pb-10" id="contact">
-      {/* background grid */}
-      <div className="w-full absolute left-0 -bottom-72 min-h-96">
-        <img
-          src="/footer-grid.svg"
-          alt="grid"
-          className="w-full h-full opacity-50 "
-        />
-      </div>
-
       <div className="flex flex-col items-center">
         <h1 className="heading lg:max-w-[45vw]">
           Ready to take <span className="text-purple">your</span> digital
