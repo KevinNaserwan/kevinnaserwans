fdfd14bbe9593d44b2f7e34f0a036e030a1dfb06
kevinnaserwan <kevinnaserwan@gmail.com>
2024-06-10 15:25:21 +0700

    Fix: bug overflow

--- data/index.ts
+++ data/index.ts
@@ -51,7 +51,7 @@ export const gridItems = [
     id: 5,
     title: "Currently building a School App With Laravel",
     description: "The Inside Scoop",
-    className: "md:col-span-3 md:row-span-2",
+    className: " md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
     titleClassName: "justify-center md:justify-start lg:justify-center",
     img: "/b5.svg",
